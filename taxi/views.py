068877876de5309ce237499ca5189180ad76c8d0
Yasha Lapin <yasha.lapin@gmail.com>
2025-04-02 15:01:59 +0300

    4

--- taxi/views.py
+++ taxi/views.py
@@ -6,8 +6,12 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm, DriverSearchForm, CarSearchForm, \
-    ManufacturerSearchForm
+from .forms import (DriverCreationForm,
+                    DriverLicenseUpdateForm,
+                    CarForm,
+                    DriverSearchForm,
+                    CarSearchForm,
+                    ManufacturerSearchForm)
 
 
 @login_required
@@ -41,7 +45,9 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
         self.queryset = self.model.objects.all()
         form = ManufacturerSearchForm(self.request.GET)
         if form.is_valid():
-            return self.queryset.filter(name__icontains=form.cleaned_data["name"])
+            return self.queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
         return self.queryset
 
 
@@ -71,7 +77,9 @@ class CarListView(LoginRequiredMixin, generic.ListView):
         self.queryset = self.model.objects.all()
         form = CarSearchForm(self.request.GET)
         if form.is_valid():
-            return self.queryset.filter(model__icontains=form.cleaned_data["model"])
+            return self.queryset.filter(
+                model__icontains=form.cleaned_data["model"]
+            )
         return self.queryset
 
 
@@ -104,7 +112,9 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
         self.queryset = self.model.objects.all()
         form = DriverSearchForm(self.request.GET)
         if form.is_valid():
-            return self.queryset.filter(username__icontains=form.cleaned_data["username"])
+            return self.queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
         return self.queryset
 
 
